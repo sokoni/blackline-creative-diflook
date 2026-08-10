4f88c8a851035adbcfa2eefe501925a805d4540a
sokoni Babb <so.babb@gmail.com>
2026-08-10 02:00:51 -0500

    Revert "Update Home page typography to crisp white matching dark design theme while preserving layout composition"
    This reverts commit 253360b1a149bb9c2c436ece5d074cf21a675526.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
     <main className="blackline-home">
 
       {/* HEADER */}
-      <Navbar variant="black" />
+      <Navbar variant="white" />
 
 
       {/* HERO */}
